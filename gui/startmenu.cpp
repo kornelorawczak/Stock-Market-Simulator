4a061da94520c8819fdb49a4b9eb89ad2461dc2e
DiOG <mfafer77@gmail.com>
2024-06-03 21:05:36 +0200

    trading instruments page, part of deposit forms

--- gui/startmenu.cpp
+++ gui/startmenu.cpp
@@ -4,11 +4,30 @@
 StartMenu::StartMenu(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::StartMenu)
+    , balance(1000.0) // balans
 {
     ui->setupUi(this);
+    ui->instrumentsButton->setStyleSheet("QPushButton { color: green; font-size: 22px; }"); //uzywanie css dla obiektu
+
+    updateBalanceDisplay();
 }
 
 StartMenu::~StartMenu()
 {
     delete ui;
+}
+
+void StartMenu::setBalance(double newBalance)
+{
+    if (balance != newBalance) {
+        balance = newBalance;
+        emit balanceChanged(balance);
+        updateBalanceDisplay();
+    }
+}
+
+void StartMenu::updateBalanceDisplay()
+{
+    ui->balanceValue->setStyleSheet("QLabel { font-size: 24px; }");
+    ui->balanceValue->setText(QString::number(balance, 'f', 2));
 }
--- gui/startmenu.h
+++ gui/startmenu.h
@@ -12,12 +12,22 @@ QT_END_NAMESPACE
 class StartMenu : public QMainWindow
 {
     Q_OBJECT
+    Q_PROPERTY(double balance READ getBalance WRITE setBalance NOTIFY balanceChanged)
 
 public:
     StartMenu(QWidget *parent = nullptr);
     ~StartMenu();
 
+    double getBalance() const { return balance; }
+    void setBalance(double newBalance);
+
+signals:
+    void balanceChanged(double newBalance);
+
 private:
     Ui::StartMenu *ui;
+    double balance;
+
+    void updateBalanceDisplay();
 };
 #endif // STARTMENU_H
