ac9e542e48f4c9fd671cbfa24e81d32e640e16b1
kornelorawczak <kornel.orawczak@gmail.com>
2024-06-04 18:41:18 +0200

    Added .monthly() function that returns data to make a chart and covered whole thing into an abstract function

--- assets.cpp
+++ assets.cpp
@@ -8,12 +8,27 @@ using namespace std;
 using namespace nlohmann;
 using namespace httplib;
 
-class Usd {
+class Data {
+private:
+    virtual void parseData(const string& jsonData, double& rate) = 0;
+    virtual double getPastValue(int days) = 0;
+    virtual string generateDateUrl(int days) = 0;
+public:
+    virtual void refreshData() = 0;
+    virtual double getValue() = 0;
+    virtual double difference() = 0;
+    virtual double* monthly() = 0;
+};
+
+
+class Currency : Data{
 private:
     string host;
     string path;
-    double usd_rate;
-    void parseData(const std::string& jsonData, double& rate) {
+    string id;
+    double current_rate;
+    double month_rates[30];
+    void parseData(const string& jsonData, double& rate) override {
         try {
             auto json = json::parse(jsonData);
             rate = json["rates"][0]["mid"].get<double>();
@@ -32,55 +47,88 @@ private:
         oss << put_time(&local_tm, "%Y-%m-%d");
         string dateStr = oss.str();
 
-        string pastURL = "/api/exchangerates/rates/A/USD/" + dateStr + "/?format=json";
+        string pastURL = "/api/exchangerates/rates/A/" + id + "/" + dateStr + "/?format=json";
         return pastURL;
     }
-    double getYesterdayValue(){
-        string yesterdayUrl = generateDateUrl(1);
+    double getPastValue(int days) override {
+        string yesterdayUrl = generateDateUrl(days);
         Client cli(host.c_str());
         auto res = cli.Get(yesterdayUrl);
-        double previousDayRate = 0.0;
+        double pastDayRate = 0.0;
         if (res && res->status == 200) {
-            parseData(res->body, previousDayRate);
+            parseData(res->body, pastDayRate);
         } else {
-            std::cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << std::endl;
+            throw exception();
+            //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
         }
 
-        return previousDayRate;
+        return pastDayRate;
     }
 public:
-    Usd() : host("api.nbp.pl"), path("/api/exchangerates/rates/A/USD/?format=json") {}
+    Currency(const string& id){
+        this->id = id;
+        this->host = "api.nbp.pl";
+        this->path = "/api/exchangerates/rates/A/" + id + "/?format=json";
+    }
 
-    void refreshData() {
+    void refreshData() override {
         Client cli(host.c_str());
         auto res = cli.Get(path.c_str());
 
         if (res && res->status == 200) {
-            parseData(res->body, usd_rate);
+            parseData(res->body, current_rate);
         } else {
             cerr << "Failed to get data from NBP API. HTTP status: " << (res ? res->status : -1) << endl;
         }
     }
 
-    double getValue(){
-        return usd_rate;
+    double getValue() override {
+        return current_rate;
     }
 
-    double difference(){
-        return getYesterdayValue();
+    double difference() override {
+        return ((current_rate - getPastValue(1))/getPastValue(1)) * 100;
+    }
+
+    double* monthly() override {
+        month_rates[0] = getValue();
+        int i = 1;
+        int j = 1;
+        while(i < 30){
+            try {
+                month_rates[i] = getPastValue(j);
+                i++;
+                j++;
+            }
+            catch(const exception &e){
+                j++;
+                continue;
+            }
+        }
+        return month_rates;
     }
 };
 
 
 int main() {
-    auto test = Usd();
-    test.refreshData();
-    cout << test.getValue() << endl;
-    cout << test.difference() << endl;
+    cout << "USD: " << endl;
+    auto usd = Currency("USD");
+    usd.refreshData();
+    cout << usd.getValue() << endl;
+    cout << usd.difference() << endl;
+    double* chart_data = usd.monthly();
+    for (int i = 0; i < 30; i++){
+        cout << "Day " + to_string((-1) * i) + ": " + to_string(chart_data[i]) << endl;
+    }
+    cout << endl << "GDP:" << endl;
+    auto gbp = Currency("GBP");
+    gbp.refreshData();
+    cout << gbp.getValue() << endl;
+    cout << gbp.difference() << endl;
+    double* chart_data2 = gbp.monthly();
+    for (int i = 0; i < 30; i++){
+        cout << "Day " + to_string((-1) * i) + ": " + to_string(chart_data2[i]) << endl;
+    }
     return 0;
 }
 
-// todo:
-// - zmień funkcje .difference() tab aby zwracała procent zmiany w ostatnich 24h
-// - funkcja .monthly() która zwraca tabele wartości ostatnich 30 dni do zrobienia wykresu
-// - owinąć to w klase bardziej abstrakcyjną assets, tak aby klasa usd to jedynie wywołanie konstruktora assets z odpowiednim linkiem
