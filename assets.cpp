330047964fc1e5c70153482d64009ae95fdc5b81
kornelorawczak <kornel.orawczak@gmail.com>
2024-06-06 13:20:43 +0200

    Added class NBPApi that's a mother class to Currency and Gold, it prevents from copying the same code multiple times

--- assets.cpp
+++ assets.cpp
@@ -12,7 +12,7 @@ class Data {
 private:
     virtual void parseData(const string& jsonData, double& rate) = 0;
     virtual double getPastValue(int days) = 0;
-    virtual string generateDateUrl(int days) = 0;
+    virtual string generateDate(int days) = 0;
 public:
     virtual void refreshData() = 0;
     virtual double getValue() = 0;
@@ -20,103 +20,15 @@ public:
     virtual double* monthly() = 0;
 };
 
-class Gold{
-private:
-    string host, path;
-    double current_rate;
-    double monthly_rates[30];
-    void parseData(const string& jsonData, double& rate)  {
-        try {
-            auto json = json::parse(jsonData);
-            rate = json[0]["cena"].get<double>();
-        } catch (const exception& e) {
-            cerr << "Failed to parse JSON data: " << e.what() << endl;
-        }
-    }
-    string generateDateUrl(int days)  {
-        auto now = chrono::system_clock::now();
-        auto past = now - chrono::hours(24*days);
-
-        time_t tt = chrono::system_clock::to_time_t(past);
-        tm local_tm = *localtime(&tt);
-
-        ostringstream oss;
-        oss << put_time(&local_tm, "%Y-%m-%d");
-        string dateStr = oss.str();
-
-        string pastURL = "/api/cenyzlota/" + dateStr + "/?format=json";
-        return pastURL;
-    }
-    double getPastValue(int days)  {
-        string pastUrl = generateDateUrl(days);
-        Client cli(host.c_str());
-        auto res = cli.Get(pastUrl);
-        double pastDayRate = 0.0;
-        if (res && res->status == 200) {
-            parseData(res->body, pastDayRate);
-        } else {
-            throw exception();
-            //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
-        }
-
-        return pastDayRate;
-    }
-public:
-    Gold() : host("api.nbp.pl"), path("/api/cenyzlota") {}
-
-    void refreshData() {
-        Client cli(host.c_str());
-        auto res = cli.Get(path.c_str());
-
-        if (res && res->status == 200) {
-            parseData(res->body, current_rate);
-        } else {
-            cerr << "Failed to get data from NBP API. HTTP status: " << (res ? res->status : -1) << endl;
-        }
-    }
-    double getValue() {
-        return current_rate;
-    }
-    double difference() {
-        return ((current_rate - getPastValue(1))/getPastValue(1)) * 100;
-    }
-    double* monthly() {
-        monthly_rates[0] = getValue();
-        int i = 1;
-        int j = 1;
-        while(i < 30){
-            try {
-                monthly_rates[i] = getPastValue(j);
-                i++;
-                j++;
-            }
-            catch(const exception &e){
-                j++;
-                continue;
-            }
-        }
-        return monthly_rates;
-    }
-};
-
-class Currency : Data{
-private:
+class NBPApi : public Data {
+protected:
     string host;
     string path;
-    string id;
-    double current_rate;
+    double current_price;
     double monthly_rates[30];
-    void parseData(const string& jsonData, double& rate) override {
-        try {
-            auto json = json::parse(jsonData);
-            rate = json["rates"][0]["mid"].get<double>();
-        } catch (const exception& e) {
-            cerr << "Failed to parse JSON data: " << e.what() << endl;
-        }
-    }
-    string generateDateUrl(int days) override {
+    string generateDate(int days) override {
         auto now = chrono::system_clock::now();
-        auto past = now - chrono::hours(24*days);
+        auto past = now - chrono::hours(24 * days);
 
         time_t tt = chrono::system_clock::to_time_t(past);
         tm local_tm = *localtime(&tt);
@@ -125,47 +37,29 @@ private:
         oss << put_time(&local_tm, "%Y-%m-%d");
         string dateStr = oss.str();
 
-        string pastURL = "/api/exchangerates/rates/A/" + id + "/" + dateStr + "/?format=json";
-        return pastURL;
+        return dateStr;
     }
-    double getPastValue(int days) override {
-        string pastUrl = generateDateUrl(days);
-        Client cli(host.c_str());
-        auto res = cli.Get(pastUrl);
-        double pastDayRate = 0.0;
-        if (res && res->status == 200) {
-            parseData(res->body, pastDayRate);
-        } else {
-            throw exception();
-            //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
-        }
 
-        return pastDayRate;
-    }
+    void parseData(const string& jsonData, double& rate) override = 0;
+    double getPastValue(int days) override = 0;
 public:
-    Currency(const string& id){
-        this->id = id;
-        this->host = "api.nbp.pl";
-        this->path = "/api/exchangerates/rates/A/" + id + "/?format=json";
-    }
-
     void refreshData() override {
         Client cli(host.c_str());
         auto res = cli.Get(path.c_str());
 
         if (res && res->status == 200) {
-            parseData(res->body, current_rate);
+            parseData(res->body, current_price);
         } else {
             cerr << "Failed to get data from NBP API. HTTP status: " << (res ? res->status : -1) << endl;
         }
     }
-
     double getValue() override {
-        return current_rate;
+        return current_price;
     }
 
     double difference() override {
-        return ((current_rate - getPastValue(1))/getPastValue(1)) * 100;
+        double yesterdayValue = getPastValue(1);
+        return ((current_price - yesterdayValue)/yesterdayValue) * 100;
     }
 
     double* monthly() override {
@@ -184,6 +78,73 @@ public:
             }
         }
         return monthly_rates;
+    }
+};
+
+
+class Currency : public NBPApi {
+private:
+    string id;
+    void parseData(const string& jsonData, double& rate) override {
+        try {
+            auto json = json::parse(jsonData);
+            rate = json["rates"][0]["mid"].get<double>();
+        } catch (const exception& e) {
+            cerr << "Failed to parse JSON data: " << e.what() << endl;
+        }
+    }
+    double getPastValue(int days) override {
+        string dateStr = generateDate(days);
+        string pastUrl = "/api/exchangerates/rates/A/" + id + "/" + dateStr + "/?format=json";
+        Client cli(host.c_str());
+        auto res = cli.Get(pastUrl);
+        double pastDayRate = 0.0;
+        if (res && res->status == 200) {
+            parseData(res->body, pastDayRate);
+        } else {
+            throw exception();
+            //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
+        }
+
+        return pastDayRate;
+    }
+public:
+    Currency(const string& id){
+        this->id = id;
+        this->host = "api.nbp.pl";
+        this->path = "/api/exchangerates/rates/A/" + id + "/?format=json";
+    }
+};
+
+class Gold : public NBPApi {
+private:
+    void parseData(const string& jsonData, double& rate) override {
+        try {
+            auto json = json::parse(jsonData);
+            rate = json[0]["cena"].get<double>();
+        } catch (const exception& e) {
+            cerr << "Failed to parse JSON data: " << e.what() << endl;
+        }
+    }
+    double getPastValue(int days) override {
+        string dateStr = generateDate(days);
+        string pastUrl = "/api/cenyzlota/" + dateStr + "/?format=json";
+        Client cli(host.c_str());
+        auto res = cli.Get(pastUrl);
+        double pastDayRate = 0.0;
+        if (res && res->status == 200) {
+            parseData(res->body, pastDayRate);
+        } else {
+            throw exception();
+            //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
+        }
+
+        return pastDayRate;
+    }
+public:
+    Gold() {
+        this->host = "api.nbp.pl";
+        this->path = "/api/cenyzlota/?format=json";
     }
 };
 
