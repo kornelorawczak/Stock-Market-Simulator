da66b19088f7468ac0af51264caef309b95c2cdb
kornelorawczak <kornel.orawczak@gmail.com>
2024-06-14 00:29:51 +0200

    Added CryptoApi class that allows to parse data from api regarding crypto currencies

--- assets.cpp
+++ assets.cpp
@@ -12,7 +12,7 @@ using namespace httplib;
 
 class Data {
 private:
-    virtual void parseData(const string& jsonData, double& rate) = 0;
+    virtual void parseData(const string& jsonData, double& rate, int type) = 0;
     virtual double getPastValue(int days) = 0;
     virtual string generateDate(int days) = 0;
 public:
@@ -22,6 +22,75 @@ public:
     virtual double difference() = 0;
     virtual double* monthly() = 0;
 };
+
+class CryptoApi : public Data {
+private:
+    const string apiKey = "2b957ad8fc306a5241947642278291a9bec7444e7bd75a7aa9fbd16981fa8597";
+    string id;
+    double currentValue = 0.0;
+    string baseUrl;
+    void parseData(const string& jsonData, double& rate, int type) override {
+        auto json = json::parse(jsonData);
+        //cout << json << endl;
+        if (type == 1) rate = json["USD"].get<double>();
+        else rate = json["BTC"]["USD"].get<double>();
+    }
+    string generateDate(int days) override {
+        time_t now = time(nullptr);
+        time_t target = now - (days * 86400);
+        ostringstream oss;
+        oss << put_time(gmtime(&target), "%s");
+        return oss.str();
+    }
+    double getPastValue(int days) override {
+        string date = generateDate(days);
+        Client cli(baseUrl);
+        string endpoint = "/data/pricehistorical?fsym=" + id + "&tsyms=USD&ts=" + date + "&api_key=" + apiKey;
+        double pastDayRate = 0.0;
+        auto res = cli.Get(endpoint);
+        if (res && res->status == 200) {
+            parseData(res->body, pastDayRate, 2);
+        }
+        else {
+            cerr << "Failed to refresh data: " << (res ? res->status : -1) << endl;
+        }
+        return pastDayRate;
+    }
+public:
+    CryptoApi(const string& name) : id(name), baseUrl("https://min-api.cryptocompare.com") {}
+
+    void refreshData() override {
+        Client cli(baseUrl);
+        string endpoint = "/data/price?fsym=" + id + "&tsyms=USD&api_key=" + apiKey;
+        auto res = cli.Get(endpoint);
+
+        if (res && res->status == 200) {
+            parseData(res->body, currentValue, 1);
+        } else {
+            throw runtime_error("Failed to refresh data from API");
+        }
+    }
+    double getValue() const override {
+        return currentValue;
+    }
+
+    double difference() override {
+        double pastValue = getPastValue(1);
+        return ((currentValue - pastValue) / pastValue) * 100;
+    }
+
+    double* monthly() override {
+        static double prices[30];
+        for (int i = 0; i < 30; ++i) {
+            prices[i] = getPastValue(i);
+        }
+        return prices;
+    }
+    string getName() const override {
+        return id;
+    }
+};
+
 
 class NBPApi : public Data {
 protected:
@@ -43,7 +112,7 @@ protected:
         return dateStr;
     }
 
-    void parseData(const string& jsonData, double& rate) override = 0;
+    void parseData(const string& jsonData, double& rate, int type) override = 0;
     double getPastValue(int days) override = 0;
 public:
     void refreshData() override {
@@ -51,7 +120,7 @@ public:
         auto res = cli.Get(path.c_str());
 
         if (res && res->status == 200) {
-            parseData(res->body, current_price);
+            parseData(res->body, current_price, 1);
         } else {
             cerr << "Failed to get data from NBP API. HTTP status: " << (res ? res->status : -1) << endl;
         }
@@ -89,7 +158,7 @@ public:
 class Currency : public NBPApi {
 private:
     string id;
-    void parseData(const string& jsonData, double& rate) override {
+    void parseData(const string& jsonData, double& rate, int type) override {
         try {
             auto json = json::parse(jsonData);
             rate = json["rates"][0]["mid"].get<double>();
@@ -104,7 +173,7 @@ private:
         auto res = cli.Get(pastUrl);
         double pastDayRate = 0.0;
         if (res && res->status == 200) {
-            parseData(res->body, pastDayRate);
+            parseData(res->body, pastDayRate, 1);
         } else {
             throw exception();
             //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
@@ -125,7 +194,7 @@ public:
 
 class Gold : public NBPApi {
 private:
-    void parseData(const string& jsonData, double& rate) override {
+    void parseData(const string& jsonData, double& rate, int type) override {
         try {
             auto json = json::parse(jsonData);
             rate = json[0]["cena"].get<double>();
@@ -140,7 +209,7 @@ private:
         auto res = cli.Get(pastUrl);
         double pastDayRate = 0.0;
         if (res && res->status == 200) {
-            parseData(res->body, pastDayRate);
+            parseData(res->body, pastDayRate, 1);
         } else {
             throw exception();
             //cerr << "Failed to get data from NBP API for the previous day. HTTP status: " << (res ? res->status : -1) << endl;
@@ -184,37 +253,13 @@ public:
 
 
 int main() {
-    /*cout << "USD: " << endl;
-    auto usd = Currency("USD");
-    usd.refreshData();
-    cout << usd.getValue() << endl;
-    cout << usd.difference() << endl;
-    double* chart_data = usd.monthly();
+    auto btc = CryptoApi("BTC");
+    btc.refreshData();
+    cout << btc.getValue() << endl;
+    cout << btc.difference() << endl;
+    double* chart_data = btc.monthly();
     for (int i = 0; i < 30; i++){
-        cout << "Day " + to_string((-1) * i) + ": " + to_string(chart_data[i]) << endl;
+        cout << "Day " << (-1) * i << ": " << chart_data[i] << endl;
     }
-    cout << endl << "GDP:" << endl;
-    auto gbp = Currency("GBP");
-    gbp.refreshData();
-    cout << gbp.getValue() << endl;
-    cout << gbp.difference() << endl;
-    double* chart_data2 = gbp.monthly();
-    for (int i = 0; i < 30; i++){
-        cout << "Day " + to_string((-1) * i) + ": " + to_string(chart_data2[i]) << endl;
-    }*/
-
-    auto gold = Gold();
-    gold.refreshData();
-    auto usd = Currency("USD");
-    usd.refreshData();
-    cout << gold.getValue() << endl;
-    cout << gold.difference() << endl;
-    /*double* chart_data3 = gold.monthly();
-    for (int i = 0; i < 30; i++){
-        cout << "Day " + to_string((-1) * i) + ": " + to_string(chart_data3[i]) << endl;
-    } */
-    Serializer::historyRecord(gold, 15.13);
-    Serializer::historyRecord(usd, 1.5);
-
     return 0;
 }
